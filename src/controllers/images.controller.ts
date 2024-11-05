ddebb7138a61e09686a843dc970ccb3a0be39c79
Muhammad Hameed <muhammadhameed105@gmail.com>
2024-11-06 00:02:44 +0500

    transform image endpoint added.

--- src/controllers/images.controller.ts
+++ src/controllers/images.controller.ts
@@ -3,6 +3,9 @@ import { Image } from '../entities/image.entity';
 import { Request, Response } from 'express';
 import { AuthenticatedRequest } from '../types/authenticated-request';
 import { User } from '../entities/user.entity';
+import sharp from 'sharp';
+import fs from 'fs/promises';
+import path from 'path';
 
 export const saveImage = async (
   req: AuthenticatedRequest,
@@ -68,6 +71,42 @@ export const getImages = async (
     data: { images },
     error: '',
     message: '',
+  };
+
+  res.json(apiResponse);
+};
+
+export const transformImage = async (
+  req: AuthenticatedRequest,
+  res: Response
+): Promise<void> => {
+  const imageId = parseInt(req.params.id, 10);
+  const imageRepository = AppDataSource.getRepository(Image);
+  const image = await imageRepository.findOneBy({ id: imageId });
+  if (!image) {
+    const apiResponse = {
+      data: {},
+      error: 'Image not found',
+      message: '',
+    };
+    res.status(404).json(apiResponse);
+    return;
+  }
+  // Transform image here
+  const transformations = req.body.transformations;
+  const imageBuffer = sharp(image.path);
+
+  const buffer = await imageBuffer.rotate().toBuffer();
+
+  // save the transformed image
+
+  await fs.writeFile(image.path, buffer); // save the transformed image to the file system
+
+  console.log('Transformations:', transformations);
+  const apiResponse = {
+    data: { image },
+    error: '',
+    message: 'Image transformed successfully',
   };
 
   res.json(apiResponse);
--- src/routes/images.route.ts
+++ src/routes/images.route.ts
@@ -1,12 +1,18 @@
 import { Router } from 'express';
 import authenticate from '../middlewares/authenticate';
 import upload from '../middlewares/multer';
-import { getImages, saveImage } from '../controllers/images.controller';
+import {
+  getImages,
+  saveImage,
+  transformImage,
+} from '../controllers/images.controller';
 
 const router = Router();
 
 router.use(authenticate);
 
 router.post('/', upload.single('image'), saveImage).get('/', getImages);
+
+router.patch('/:id/transform', transformImage);
 
 export default router;
